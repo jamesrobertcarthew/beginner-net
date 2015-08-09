fixed synapse shape and forward prop loop seems to be happy.. back to backprop

second_generation.py
@@ -30,21 +30,25 @@ class second_generation(object):
     def forward_propagation(self, layer):
         self.log('Entering Forward Propagation Loop')
         layer[0] = self.data_in
+        self.log("Layer {!s}".format(0), layer[0])
+        self.log("Synapse {!s}".format(0), self.synapse[0])
         for j in range(0, self.layer_count-1):
-            self.log("Layer {!s}".format(j), layer[j])
-            self.log("Synapse {!s}".format(j), self.synapse[j])
-            layer[j+1] = np.dot(layer[0], self.synapse[j])
+            layer[j+1] = np.dot(layer[j], self.synapse[j])
+            self.log("Layer {!s}".format(j+1), layer[j+1])
+            self.log("Synapse {!s}".format(j+1), self.synapse[j+1])
         return layer
 
     def backpropagation(self, layer, delta, error):
         self.log('Entering Backpropagation Loop')
         error[self.layer_count-1] = self.desired_output - layer[self.layer_count-1]
         delta[self.layer_count-1] = error[self.layer_count-1] * self.derivative_of_sigmoid(layer[self.layer_count-1])
+        self.log('Error {!s}'.format(0), error[0])
+        self.log('Confidence Weighted Error {!s}'.format(0), delta[0])
         for j in reversed(range(1, self.layer_count)):
-            self.log('Error {!s}'.format(j), error[j])
-            self.log('Confidence Weighted Error {!s}'.format(j), delta[j])
             error[j-1] = np.dot(delta[j], self.synapse[j-1].T)
             delta[j-1] = error[j-1] * self.derivative_of_sigmoid(layer[j-1])
+            self.log('Error {!s}'.format(j), error[j])
+            self.log('Confidence Weighted Error {!s}'.format(j), delta[j])
 
         return layer, delta, error
 
@@ -54,4 +58,4 @@ class second_generation(object):
         delta = [None]*self.layer_count
         for i in xrange(iterations):
             layer = self.forward_propagation(layer)
-            layer, delta, error = self.backpropagation(layer, delta, error)
+            # layer, delta, error = self.backpropagation(layer, delta, error)