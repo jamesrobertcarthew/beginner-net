fa1024c97bfa0016bf5a7d4d97874c8e9e6d77c1
James Carthew <jamesrobertcarthew@gmail.com>
2015-08-24 00:14:06 +1000

    need to figure out how to organise this thing better

--- simple_net.py
+++ simple_net.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pickle
+import os
+import inspect
 
 
 class simple_net(object):
 
-    # Setup Class Variables
     def __init__(self, seed=1, verbose=False):
+        # Setup Class Variables
         self.verbose = verbose
         self.data_in = []
         self.desired_output = []
@@ -16,23 +18,24 @@ class simple_net(object):
         self.dataset_gain = 1
         self.dataset_bias = 0
         self.mode = 'RAW'  # ,SCALED, ASCII, ...
+        self.path = os.path.abspath(inspect.getfile(self.__class__))
 
-    # Set Float Precision in Logging
     def do_logging_prettier(self):
+        # Set Float Precision in Logging
         np.set_printoptions(precision=3)
         np.set_printoptions(suppress=True)
         np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
 
-    # Guess what this does
     def log(self, a_string='Value', data=None):
+        # Guess what this does
         if self.verbose is True:
             if data is None:
                 print '\n{!s}\n'.format(a_string)
             else:
                 print '\n{!s}: \n{!s}'.format(a_string, str(data))
 
-    # Output the result and desired output for comparison
     def give_result(self):
+        # Output the result and desired output for comparison
         store_verbose_setting = self.verbose
         self.verbose = True
         error = self.desired_output - self.actual_raw_output
@@ -56,27 +59,27 @@ class simple_net(object):
         return self.actual_raw_output
 
     def get_scaled_output(self):
-        self.log('Scaled Net Output', self.denormalise_dataset_linear(self.actual_raw_output))
-        self.log('Scaled Desired Output', self.denormalise_dataset_linear(self.desired_output))
-        return self.denormalise_dataset_linear(self.actual_raw_output)
+        self.log('Scaled Net Output', self.restore_characteristics(self.actual_raw_output))
+        self.log('Scaled Desired Output', self.restore_characteristics(self.desired_output))
+        return self.restore_characteristics(self.actual_raw_output)
 
     def get_ascii_output(self):
-        scaled_data_out = self.denormalise_dataset_linear(self.actual_raw_output)
-        scaled_desired_out = self.denormalise_dataset_linear(self.desired_output)
+        scaled_data_out = self.restore_characteristics(self.actual_raw_output)
+        scaled_desired_out = self.restore_characteristics(self.desired_output)
         self.log('Ascii Net Output', self.float_to_ascii(scaled_data_out))
         self.log('Ascii Desired Output', self.float_to_ascii(scaled_desired_out))
         return self.float_to_ascii(scaled_data_out)
 
-    # Sigmoid Function maps input to values between 0 and 1
     def sigmoid(self, x):
+        # Sigmoid Function maps input to values between 0 and 1
         return 1 / (1 + np.exp(-x))
 
-    # Derivative of Sigmoid gives measure of confidence to be applied to calculated error during training
     def derivative_of_sigmoid(self, layer):
+        # Derivative of Sigmoid gives measure of confidence to be applied to calculated error during training
         return layer * (1 - layer)
 
-    # Full Batch Prediction Function
     def forward_propagation(self, layer, iteration=0):
+        # Full Batch Prediction Function
         self.log('Entering Forward Propagation Loop', iteration)
         layer[0] = self.data_in
         self.log('Layer {!s}'.format(0), layer[0])
@@ -88,8 +91,8 @@ class simple_net(object):
             self.log('Synapse {!s}'.format(j+1), self.synapse[j+1])
         return layer
 
-    # Full Batch Update Backpropgation Function
     def backpropagation(self, layer, delta, error, iteration=0):
+        # Full Batch Update Backpropgation Function
         self.log('Entering Backpropagation Loop', iteration)
         error[self.layer_count-1] = self.desired_output - layer[self.layer_count-1]
         delta[self.layer_count-1] = error[self.layer_count-1] * self.derivative_of_sigmoid(layer[self.layer_count-1])
@@ -102,23 +105,28 @@ class simple_net(object):
             self.log('Error {!s}'.format(j), error[j])
         return layer, delta, error
 
-    # Update the Synapses based on the confidence of the value. Less confident => 'more updated'
     def update_synapses(self, layer, delta):
+        # Update the Synapses based on the confidence of the value. Less confident => 'more updated'
         self.log('Entering Update Synapse Loop')
         for j in range(0, self.layer_count-1):
             self.synapse[j] += np.dot(layer[j].T, delta[j+1])
 
-# Loop for Full Batch Backpropgation Training
+    def initialise_synapse(self):
+        # Initialise the Synapse structure
+        self.synapse.append(2*np.random.random((self.data_in.shape[1], self.desired_output.shape[0])) - 1)
+        for i in xrange(self.layer_count):
+            self.synapse.append(2*np.random.random((self.desired_output.shape[0], self.desired_output.shape[1])) - 1)
+            self.synapse.append(2*np.random.random((self.desired_output.shape[1], self.desired_output.shape[0])) - 1)
+
     def train(self, layer_count, iterations, data_in=None, desired_output=None):
+        # Loop for Full Batch Backpropgation Training
         self.log('Entering Training Loop')
         if data_in is not None or desired_output is not None:
             self.data_in = data_in
             self.desired_output = desired_output
         self.layer_count = layer_count
-        self.synapse.append(2*np.random.random((self.data_in.shape[1], self.desired_output.shape[0])) - 1)
-        for i in xrange(self.layer_count):
-            self.synapse.append(2*np.random.random((self.desired_output.shape[0], self.desired_output.shape[1])) - 1)
-            self.synapse.append(2*np.random.random((self.desired_output.shape[1], self.desired_output.shape[0])) - 1)
+        if self.synapse == []:
+            self.initialise_synapse()
         layer = [None] * (self.layer_count)
         error = [None] * (self.layer_count)
         delta = [None] * (self.layer_count)
@@ -129,35 +137,37 @@ class simple_net(object):
         self.actual_raw_output = layer[self.layer_count-1]
         self.give_result()
 
-    # Run Net, Requires a Synapse Array
-    def run(self, data_in, layer_count, iterations):
+    def run(self, layer_count, iterations, data_in=None):
+        # Run Neural Net, Requires a Synapse Array
+        if data_in is not None:
+            self.data_in = data_in
         store_verbose_setting = self.verbose
-        if self.synapse is None:
-            self.log('Error', 'Please Load a Synapse')
+        if self.synapse == []:
+            self.log('Program Error', 'Please Load a Synapse')
             exit()
         self.layer_count = layer_count
         layer = [None] * (self.layer_count)
-        layer[0] = data_in
+        layer[0] = self.data_in
         for i in xrange(iterations):
             layer = self.forward_propagation(layer, i)
         self.actual_raw_output = layer[self.layer_count-1]
         self.give_result()
         return self.actual_raw_output
 
-    # Save Synapse for later use
     def save_synapse(self, file_name):
+        # Save Synapse for later use
         self.log('Save Synapse', file_name)
         file_object = open(file_name, 'wb')
         pickle.dump(self.synapse, file_object)
 
-    # Load a Synapse for reuse
     def load_synapse(self, file_name):
+        # Load a Synapse for reuse
         self.log('Load Synapse', file_name)
         file_object = open(file_name, 'r')
         self.synapse = pickle.load(file_object)
 
-    # Scale input and output datasets to (0,1) linearly using scaled_data = k * data - bias
-    def normalise_dataset_linear(self, data_in, desired_output):
+    def digest_float(self, data_in, desired_output):
+        # Scale input and output datasets to (0,1) linearly using scaled_data = k * data - bias
         self.mode = 'SCALED'
         self.log('Linearly Scale Data')
         self.dataset_gain = np.ceil(max(np.nanmax(data_in), np.nanmax(desired_output)) - min(np.nanmin(data_in), np.nanmin(desired_output)))
@@ -171,35 +181,36 @@ class simple_net(object):
         self.log('Original Desired Output', desired_output)
         self.log('Scaled Desired Output', self.desired_output)
 
-    # Unscale the dataset after net
-    def denormalise_dataset_linear(self, data):
+    def restore_characteristics(self, data):
+        # Unscale the dataset after net
         return (self.dataset_gain * data) - self.dataset_bias
 
-    # Read in Ascii values and scale input / output for text
-    # NB: currently requires all values in input strings to be same length
-    def digest_ascii(self, data_in, desired_output):
+    def digest_ascii(self, data_in, desired_output=None):
+        # Read in Ascii values and scale input / output for text
+        # NB: currently requires all values in input strings to be same length
         self.log('Digest Ascii Array - Data In', str(data_in))
         self.log('Digest Ascii Array - Desired Output', str(desired_output))
         self.mode = 'ASCII'
         self.dataset_gain = 127
         self.data_in = self.ascii_to_float(data_in)
-        self.desired_output = self.ascii_to_float(desired_output)
+        if desired_output is not None:
+            self.desired_output = self.ascii_to_float(desired_output)
         self.log('Mapped Ascii Data In', self.data_in)
         self.log('Mapped Ascii Desired Output', self.desired_output)
 
-    # Convert an ascii array to a (normalised) float array
     def ascii_to_float(self, ascii_array):
+        # Convert an ascii array to a (normalised) float array
         float_representation = []
         for data in ascii_array:
             catcher = []
-            for cell in data:
-                for character in cell:
-                    catcher.append((ord(character))/127.0)  # Cause Float
+            for string in data:
+                for character in string:
+                    catcher.append((ord(character))/127.0)
             float_representation.append(np.asarray(catcher))
         return np.asarray(float_representation)
 
-    # Convert a normalised Float array to ascii characters
     def float_to_ascii(self, data):
+        # Convert a normalised Float array to ascii characters
         string_representation = []
         for row in data:
             a_string = ""
@@ -207,3 +218,9 @@ class simple_net(object):
                 a_string += (chr(int(value + 0.1)))  # HACK: Values occasionally approac limit .999
             string_representation.append(a_string)
         return np.asarray(string_representation)
+
+# TODO: lets make this thing into more, smaller files and stuff... is getting out of control!!!
+# TODO: Make the default mode 'run til convergence' with option to overtrain by setting iteration value (think about this and commit before hand cause you WILL fuck it up
+# TODO: gradient descent and drop output
+# TODO: CUDA dot product
+# TODO: Seperate net from main loop via sockets
--- test.py
+++ test.py
@@ -7,38 +7,35 @@ import os  # not neccesary, just a useful delineator :-P #
 os.system('echo Go, Neural Network, Go! | cowsay | lolcat')  #
 #########################################################
 
+# SETUP VARS
 # data_in = np.array([[0,0,-2,0],[0,1,1,0],[1,0,8,1],[1,1,1,1]])
-
 # desired_output = np.array([[0, 1, 1, 1],[1, 0, 1, 0],[1, -1, 5, 1],[0, 0, 0, 1]])
-
 chars_in = np.array([['a bag bites a cat'], ['a hat bites a bat'], ['a cat bites a hat']])
-
 desired_chars_out = np.array([['a cat is not a bat'], ['a dog is not a hat'], ['a rat is not a cat']])
-
 layer_count = 5
-
 seed = 1
-
 verbose = True
-
+iterations = 200000
+# SETUP SIMPLE NET
 my_net = simple_net(seed, verbose)
-
 my_net.do_logging_prettier()
 
+# READ DATA FOR TRAINING
 my_net.digest_ascii(chars_in, desired_chars_out)
-
-# my_net.normalise_dataset_linear(data_in, desired_output)
-
-iterations = 200000
-
-my_net.verbose = False
-
+# my_net.digest_float(data_in, desired_output)
+my_net.verbose = False  # just cause it talks a lot and is slow with verbose training
+# TRAIN
 my_net.train(layer_count, iterations)
-
+# SAVE SYNAPSE AND RELOAD SYNAPSE
 my_net.save_synapse('atest.synapse')
-
 my_net.load_synapse('atest.synapse')
-
-# my_net.run(data_in, layer_count, iterations)
-
-# TODO: create more explicit input for my_net.run so I can start some large dataset randomness stuff
+# RUN TEST 1
+desired_chars_out = np.array([['test 1            ']])
+chars_in = np.array([['a hat bites a bat'], ['a bag bites a cat'], ['a cat bites a hat']])
+my_net.digest_ascii(chars_in, desired_chars_out)
+my_net.run(layer_count, iterations)
+# RUN TEST 2
+desired_chars_out = np.array([['test 2            ']])
+chars_in = np.array([['a hat bites a bat']])
+my_net.digest_ascii(chars_in, desired_chars_out)
+my_net.run(layer_count, iterations)
