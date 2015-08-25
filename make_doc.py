906cd22c9f68266e74a0a2aab228af883f9392cf
James Carthew <jamesrobertcarthew@gmail.com>
2015-08-25 12:16:36 +1000

    testing git hook

--- make_doc.py
+++ make_doc.py
@@ -1,11 +1,17 @@
 import os
 directory_list = []
 already_todo = False
-with open('output.md', 'wb') as doc:
+preamble = ''
+with open('readme.txt', 'r') as notes:
+    preamble = notes.read()
+
+with open('README.md', 'wb') as doc:
+    doc.write(preamble)
+    doc.write('#Overview:\n')
     for file_ in os.listdir(os.getcwd()):
-        if file_.endswith('.py') and file_ != 'make_doc.py':
+        if file_.endswith('.py') and file_ != 'make_doc.py' and file_ != 'test.py':
             with open(file_, 'r') as code:
-                doc.write('#{!s}\n'.format(file_))
+                doc.write('##{!s}\n'.format(file_))
                 text = code.read()
                 lines = text.split('\n')
                 for line in lines:
@@ -15,11 +21,11 @@ with open('output.md', 'wb') as doc:
                 for line in lines:
                     line = line.lstrip().replace('_', '\_')
                     if line[0:3] == 'def':
-                        doc.write('## {!s}\n'.format(line.replace('def ', '')))
+                        doc.write('### {!s}\n'.format(line.replace('def ', '')))
                     if line[0:1] == '#':
                         if line[0:7] == '# TODO:':
                             if already_todo is False:
-                                doc.write('## TO DO:\n')
+                                doc.write('### TO DO:\n')
                                 already_todo = True
                             doc.write('*{!s}\n'.format(line.replace('# TODO:', '')))
                         else:
